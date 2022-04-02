f85a8f1efcb63de827c028a6633fae811e0605ad
Vikrant <vikrant2812@gmail.com>
2022-04-03 03:14:53 +0530

    feat : ghomepage added

---
+++ my-app/src/components/VideoCard/ViCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./ViCard.css"
+
+const ViCard = ({video}) => {
+    const {_id,title,category} = video;
+  return (
+    <div className="parent">
+        <div className="card">
+            <div className="image">
+                <img src={`https://i.ytimg.com/vi/${_id}/hqdefault.jpg`} alt="carosol"/>
+            </div>
+            <div className="title">
+                <h2 className="title__header">{title}</h2>
+                <h4 className="title__subheading">{`Category : ${category}`}</h4>
+            </div>
+            <div className="content">
+            <i style={{color:"red"}} className="bi bi-bookmark"></i>
+                <i className="bi bi-bookmark-fill"></i>
+                {/* <p className="content__color">Visit ten places on our planet that are undergoing the biggest change today.</p> */}
+            </div>
+        </div>
+       
+</div>
+  )
+}
+
+export default ViCard
