442eee7fe28798accdf7eecff2f4096e28e30f84
Vikrant <vikrant2812@gmail.com>
2022-04-04 07:43:59 +0530

    fix : format spacing

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
-import {BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import { VideoProvider } from "./context/context";
 
 // Call make Server
